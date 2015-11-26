eca0a50d23ae82775d7b6cd2cf31f91a15e73338
WID SOGATA <wsogata@gmail.com>
2015-11-25 17:10:34 -0800

    commit 2

--- MSDS7330-Project/python/hl7_load.py
+++ MSDS7330-Project/python/hl7_load.py
@@ -1,8 +1,7 @@
-# MSDS7730
 import mysql.connector as sqlconn
 import hl7
 from datetime import datetime
-"""
+
 db = sqlconn.connect(host="haswell9",user="wid", passwd="oracle01")
 dbc = db.cursor()
 dbc.execute("CREATE DATABASE IF NOT EXISTS hl7db")
@@ -16,27 +15,29 @@ dbc.execute("DROP VIEW IF EXISTS tab_info")
 dbc.execute("CREATE TABLE patients ( \
                 patient int(8) not null auto_increment primary key, \
                 zipcode varchar(10), \
-                city varchar(25),\
+                city varchar(35),\
                 state varchar(3),\
-                sex varchar(2).\
+                sex varchar(2),\
                 race varchar(15),\
-                marital varchar(10),\
-                birth_pl varchar(20),\)")
+                marital varchar(15),\
+                birth_pl varchar(35))")
+dbc.execute("CREATE TABLE facilities ( \
+                name varchar(35) not null primary key,\
+                address varchar(35),\
+                zipcode varchar(10),\
+                city varchar(25),\
+                state varchar(3))")                
 dbc.execute("CREATE TABLE events( \
+                event int(8) not null auto_increment primary key,\
                 patient int(8), \
                 admit_time date, \
                 disc_time date,\
                 facility varchar(20),\
-                FOREIGN KEY(patient) REFERENCES patients(patient)\
-                ON DELETE CASCADE,\
-                FOREIGN KEY(facility)REFERENCES facilities(name)\
-                ON DELETER CASCADE)")
-dbc.execute("CREATE TABLE facilities ( \
-                name varchar(20) primary key,\
-                address varchar(20),\
-                zipcode varchar(10),\
-                city varchar(15),\
-                state varchar(3))")
+                FOREIGN KEY fk_patients (patient) REFERENCES patients(patient)\
+                ON DELETE CASCADE ON UPDATE CASCADE,\
+                FOREIGN KEY fk_facility (facility) REFERENCES facilities(name)\
+                ON DELETE CASCADE ON UPDATE CASCADE)")
+
 dbc.execute ("CREATE VIEW tab_info \
                 AS select zipcode, count(p.zipcode) count\
                 FROM patients p\
@@ -45,52 +46,58 @@ dbc.execute ("CREATE VIEW tab_info \
 dbc.execute("commit")
 
 
-for line in f:
-    h = hl7.parse(line)
-    print h
-    bdate = h[1][0]
-    print bdate
-
-""" 
 f = open ('AnonymizedA01')
+#f = open ('Ano1')
+
 for line in f:
-    h = hl7.parse(line)
-  # patients    
-    p_zip =   str(h[2][11][0][4])
-    p_city =  str(h[2][11][0][2])
-    p_state = str(h[2][11][0][3])
-    p_sex =   str(h[2][8]) 
-    p_death = str(h[2][30])
-    p_race =  str(h[2][10])
-    p_marital = str(h[2][16])
-    p_birth_pl =   str(h[2][23])
-    dob = str(h[2][7])
-    p_dob = datetime.strptime(dob[0:8], '%Y%m%d')
-     
-  # events
-    admit = str(h[1][2])
-    p_admit = datetime.strptime(admit[0:8], '%Y%m%d')
-    dischg = str(h[1][4])    
-    e_dischg = datetime.strptime(dischg[0:8], '%Y%m%d')
+    try:
+        h = hl7.parse(line)
+      # patients    
+        p_zip =     str(h[2][11][0][4])
+        p_city =    str(h[2][11][0][2])
+        p_state =   str(h[2][11][0][3])
+        p_sex =     str(h[2][8]) 
+        p_death =   str(h[2][30])
+        p_race =    str(h[2][10])
+        p_marital = str(h[2][16])
+        p_birth_pl= str(h[2][23][0][2])       
+        dob =       str(h[2][7])
+        p_dob = datetime.strptime(dob[0:8], '%Y%m%d')
 
-  # facilities
-    f_name = str(h[0][4])
-#    print f_name
-    f_zip = "90210"
+      # events
+        admit = str(h[1][2])
+        p_admit = datetime.strptime(admit[0:8], '%Y%m%d')
+        dischg = str(h[1][4])  
+    #    e_dischg = datetime.strptime(dischg[0:8], '%Y%m%d')
+    #    e_fname = str(h[1][5][0][8])
 
-    dbc.execute ("INSERT INTO patients (zipcode,city,state,sex,ethnic,marital,birth_pl)\
+    
+      # facilities
+    #    f_name = str(h[3][3][0][0])       
+    #    f_addr = str(h[4][11])
+    #    f_city = str(h[4][11][0][2])
+    #    f_zip = str(h[4][11][0][4])
+        f_name = "CSMC"
+        f_addr = "8700 Beverly Blvd."
+        f_city = "Los Angeles"
+        f_zip  = "90048"
+  
+    except IndexError:
+        pass
+
+    dbc.execute ("INSERT INTO patients (zipcode,city,state,sex,race,marital,birth_pl)\
                   VALUES (%s,%s,%s,%s,%s,%s,%s)",[p_zip,p_city,p_state,p_sex,p_race,\
                   p_marital,p_birth_pl])    
-    dbc.execute ("INSERT INTO facilities (name, address, zipcode, city)\
-                  VALUES (%s,%s,%s,%s), f_name, )
-    dbc.execute ("INSERT INTO events (admit_time, fac)\
-                  VALUES (%s,%s)",[admit_dt, f_zip])   
-                  
+    dbc.execute ("INSERT INTO events (admit_time)\
+                  VALUES (%s)",[p_admit])   
 
-dbc.execute("commit")    
-    
+
+dbc.execute ("INSERT INTO facilities (name, address, city, zipcode)\
+              VALUES (%s,%s,%s,%s)", [f_name,f_addr,f_city,f_zip])                 
+dbc.execute("commit")
+
+ 
 f.close()
-
 
 
 """
