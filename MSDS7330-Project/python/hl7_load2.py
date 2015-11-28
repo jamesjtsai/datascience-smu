a0c955589186f6703ed6388cc3ba17b66a84891c
James Tsai <james.j.tsai@gmail.com>
2015-11-27 23:08:06 -0800

    added random number generator.

--- MSDS7330-Project/python/hl7_load2.py
+++ MSDS7330-Project/python/hl7_load2.py
@@ -1,6 +1,7 @@
 # MSDS7730
 import mysql.connector as sqlconn
 import hl7
+import random
 from datetime import datetime
 from datetime import date
 
@@ -26,52 +27,51 @@ dbc.execute("CREATE TABLE patients ( \
                 age int(5))")
 dbc.execute("commit")
 
-
+def genRandNumber(z):
+    return random.randint(1,z)
+    
 def calculate_age(born):
     today = date.today()
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
-inserts = 1
+inserts = 0
+count = 0
+freq = genRandNumber(100)
 f = open ('ersh1700_20151117.dat.A04')
 for line in f:
-    count =+ 1
+    count = count+1
     try:
-        h = hl7.parse(line)
+        if (count%100 == freq):
+            h = hl7.parse(line)
+            #patient demographics
+            p_zip = str(h[2][11][0][4])
+            p_city =  str(h[2][11][0][2])
+            p_state = str(h[2][11][0][3])
+            p_sex =   str(h[2][8])
+            p_death = str(h[2][30])
+            p_race =  str(h[2][10])
+            p_marital = str(h[2][16])
+            p_birth_pl =   str(h[2][23])
+            dob = str(h[2][7])
+            if len(p_state) > 20:
+                p_state = p_state[:20]
+            if len(p_race) > 15:
+                p_race = "Other"
+            if len(p_marital) == 0:
+                p_marital = 'U'
+            else:
+                p_marital = p_marital[0]
+            p_age = -1
+            if len(dob) >= 8:
+                dob = dob[:8]
+            p_age = calculate_age(datetime.strptime(dob, '%Y%m%d'))
+            dbc.execute ("INSERT INTO patients (zipcode,city,state,sex,race,marital,age)\
+            VALUES (%s,%s,%s,%s,%s,%s,%s)",[p_zip,p_city,p_state,p_sex,p_race,p_marital,p_age])
+            inserts += 1
+            print "Inserted",inserts,"rows."
     except:
         print "could not parse message"
-        continue
-    # patient demographics
-    p_zip = str(h[2][11][0][4])
-    p_city =  str(h[2][11][0][2])
-    p_state = str(h[2][11][0][3])
-    p_sex =   str(h[2][8]) 
-    p_death = str(h[2][30])
-    p_race =  str(h[2][10])
-    p_marital = str(h[2][16])
-    p_birth_pl =   str(h[2][23])
-    dob = str(h[2][7])
-
-    if len(p_state) > 20:
-        p_state = p_state[:20]
-    
-    if len(p_race) > 15:
-        p_race = "Other"
-    
-    if len(p_marital) == 0:
-        p_marital = 'U'
-    else:
-        p_marital = p_marital[0]
-    
-    p_age = -1
-    if len(dob) >= 8:
-        dob = dob[:8]
-        p_age = calculate_age(datetime.strptime(dob, '%Y%m%d'))
-
-    dbc.execute ("INSERT INTO patients (zipcode,city,state,sex,race,marital,age)\
-    VALUES (%s,%s,%s,%s,%s,%s,%s)",[p_zip,p_city,p_state,p_sex,p_race,p_marital,p_age])
-
-    inserts += 1
-    print "Inserted",inserts,"rows."
+        pass
 f.close()
 dbc.execute("commit")
 
